3ac9b4f8f8d2309bb328a91b7689e25118ded61f
Ten-T-Tonmoy <stu01754@gmail.com>
2025-03-28 22:51:45 +0600

    templating engine sucks

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 import express from "express";
 import axios from "axios";
 import nodemon from "nodemon"; // no need tho
+import path from "path";
+import { fileURLToPath } from "url";
 
-
+//bruh in module u cant just directly use __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = 3000;
 const app = express();
+app.set("view engine", "ejs");
+// view directory setting up
+app.set("views", path.join(__dirname, "views"));
+//path join current server directory/public
+//static files will go from here!
+app.use(express.static(path.join(__dirname, "public")));
+// path join gets the
 
 //----------------------------endpoints--------------------------------------------
-app.get("/");
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "This is a memphis",
+  });
+});
 
 app.listen(PORT || 3000, () => {
   console.log(`Running on port ${PORT}`);
